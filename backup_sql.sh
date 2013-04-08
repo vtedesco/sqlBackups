5f69d2618ddea560c0999aeec6f38b06638c3f0c
Vivien Tedesco <vivien.tedesco@gmail.com>
2013-04-08 10:40:49 +0300

    Variable addition for efficiency

--- backup_sql.sh
+++ backup_sql.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # Forked from http://www.mon-code.net/article/2/script-shell-de-sauvegarde-des-bases-de-donnees-mysql
 
+# Connection variables
+USER='backupUser'
+PASS='password'
+HOST='localhost'
+
 # Date Format
 DATE=`date +%y-%m-%d`
 
-LISTEBDD=$( echo 'show databases' | mysql -uroot -p****** )
+LISTEBDD=$( echo 'show databases' | mysql -u$USER -p$PASS -h$HOST)
 
 # For each DB
 for SQL in $LISTEBDD
@@ -14,7 +19,7 @@ do
   if [ $SQL != "information_schema" ] && [ $SQL != "mysql" ] && [ $SQL != "Database" ]; then
   
     mkdir -p /var/backups/sql/$DATE
-    mysqldump -uroot -p****** $SQL | gzip > /var/backups/sql/$DATE/$SQL.sql.gz
+    mysqldump -u$USER -p$PASS -h$HOST $SQL | gzip > /var/backups/sql/$DATE/$SQL.sql.gz
   
   fi
 
